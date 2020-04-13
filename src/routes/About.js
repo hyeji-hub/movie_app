23844e3c630fa6d163b8a50346e1a52ab99a567b
hyejichoi <choihyeji2952@gmail.com>
2020-04-13 14:25:24 +0900

    update

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="textBox">
+            <span className="text">About this page</span>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    componentDidMount() {
+
+    };
+    render() {
+        return <span>Hello</span>
+    };
+};
+
+export default Detail;
